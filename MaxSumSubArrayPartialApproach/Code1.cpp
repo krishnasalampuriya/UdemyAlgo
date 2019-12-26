3440bb7c8548063a82283a2e0bfc2712facebb8a
Krishna Ashok Salampuriya <krishnasalampuriya2000@gmail.com>
2019-12-26 11:34:24 +0000

    complet code

--- MaxSumSubArrayPartialApproach/Code1.cpp
+++ MaxSumSubArrayPartialApproach/Code1.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
-usinng namespace std;
+using namespace std;
+const int Nmax = 10001;
+int maxSum(int a[Nmax],int length){
+    int s[Nmax];
+    s[1]=a[1];
+    for(int i=2;i<=length;i++){
+        s[i]= a[i]+s[i-1];
+    }
+    int sum=s[1],mins=0;
+    for (int i=1; i<=length; i++) {
+        if(s[i]-mins>sum){
+            sum=s[i]-mins;    
+        }
+        if(mins>s[i])
+            mins=s[i];
+
+    }
+    return sum;
+}
 int main(){
+    int length,a[Nmax]; 
+    cout<<"Enter the length of Array";
+    cin>>length;
+    for(int i=1;i<=length;i++)
+        cin>>a[i];
+    cout<<"Maximum sum of Sub Array is : "<<maxSum(a,length);
   return 0;
 }
