551066c08262780b2a05f94c7992554f76d5f845
Krishna Ashok Salampuriya <krishnasalampuriya2000@gmail.com>
2019-12-30 07:08:25 +0000

    change

--- LongestconsecutiveSubArray/Code1.cpp
+++ LongestconsecutiveSubArray/Code1.cpp
@@ -4,19 +4,31 @@ const int Nmax=1001;
 int a[Nmax],n;
 bool fr[Nmax];
 int longestConsecutiveArray(){
+    int ans=0;
     for(int Left=1;Left<=n;Left++){
         int Min=a[Left],Max=a[Left];
         for(int i=1;i<=n;i++)
             fr[i]=false;
         for(int Right =Left;Right<=n;Right++){
-            if(fr[Right]==true)
+            if(fr[a[Right]]==true)
                 break;
-            fr[Right]=true;
+            fr[a[Right]]=true;
             Max=max(Max,a[Right]);
-            Min=mi
+            Min=min(Min,a[Right]);
+            if(Max-Min==Right -Left)
+                ans=max(Right-Left+1,ans);
         }
     }
+    return ans;
 }
 int main(){
+    
+    
+    cout <<"Enter the Length of Array : ";
+    cin>>n;
+    cout<<"Enter the elements ";
+    for(int i=1;i<=n;i++)
+        cin>>a[i];
+    cout<<longestConsecutiveArray();
     return 0;
 }
---
+++ LongestconsecutiveSubArray/test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10,b=20,c;
+    c=max(a,b);
+    int d=min(a,b);
+    cout << c<<" "<<d;
+    return 0;
+}
